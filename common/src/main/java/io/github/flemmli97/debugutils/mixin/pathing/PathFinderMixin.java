08a4b16797752345a71cdd2a49e24cae2da8c413
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-15 21:28:08 +0200

    better path debug handling. fixes potential crash if not present

--- common/src/main/java/io/github/flemmli97/debugutils/mixin/pathing/PathFinderMixin.java
+++ common/src/main/java/io/github/flemmli97/debugutils/mixin/pathing/PathFinderMixin.java
@@ -1,6 +1,7 @@
 package io.github.flemmli97.debugutils.mixin.pathing;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import io.github.flemmli97.debugutils.DebugToggles;
+import io.github.flemmli97.debugutils.utils.PathFindDebugData;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.PathNavigationRegion;
@@ -12,6 +13,7 @@ import net.minecraft.world.level.pathfinder.Target;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -23,15 +25,31 @@ import java.util.Set;
  * Sets data necessary for the path debug packets
  */
 @Mixin(PathFinder.class)
-public class PathFinderMixin {
+public abstract class PathFinderMixin implements PathFindDebugData {
 
     @Final
     @Shadow
     private BinaryHeap openSet;
 
-    @Inject(method = "findPath(Lnet/minecraft/world/level/PathNavigationRegion;Lnet/minecraft/world/entity/Mob;Ljava/util/Set;FIF)Lnet/minecraft/world/level/pathfinder/Path;", at = @At("TAIL"))
-    private void setPathDebugs(PathNavigationRegion region, Mob mob, Set<BlockPos> targetPositions, float maxRange, int accuracy, float searchDepthMultiplier, CallbackInfoReturnable<Path> info,
-                               @Local Map<Target, BlockPos> map) {
-        ((PathAccessor) info.getReturnValue()).debugData(this.openSet.getHeap(), new Node[0], map.keySet());
+    @Unique
+    private Path.DebugData debugutils$lastData;
+
+    @Inject(method = "findPath(Lnet/minecraft/world/level/PathNavigationRegion;Lnet/minecraft/world/entity/Mob;Ljava/util/Set;FIF)Lnet/minecraft/world/level/pathfinder/Path;",
+            at = @At("HEAD"))
+    private void clearDebugs(PathNavigationRegion region, Mob mob, Set<BlockPos> targetPositions, float maxRange, int accuracy, float searchDepthMultiplier, CallbackInfoReturnable<Path> info) {
+        this.debugutils$lastData = null;
+    }
+
+    @Inject(method = "findPath(Lnet/minecraft/world/level/pathfinder/Node;Ljava/util/Map;FIF)Lnet/minecraft/world/level/pathfinder/Path;",
+            at = @At("TAIL"))
+    private void setPathDebugs(Node node, Map<Target, BlockPos> targetPos, float maxRange, int accuracy, float searchDepthMultiplier, CallbackInfoReturnable<Path> info) {
+        if (DebugToggles.DEBUG_PATHS.get()) {
+            this.debugutils$lastData = new Path.DebugData(this.openSet.getHeap(), new Node[0], targetPos.keySet());
+        }
+    }
+
+    @Override
+    public Path.DebugData getLastData() {
+        return this.debugutils$lastData;
     }
 }
---
+++ common/src/main/java/io/github/flemmli97/debugutils/utils/PathFindDebugData.java
@@ -0,0 +1,8 @@
+package io.github.flemmli97.debugutils.utils;
+
+import net.minecraft.world.level.pathfinder.Path;
+
+public interface PathFindDebugData {
+
+    Path.DebugData getLastData();
+}
